ca020342601d011d07eb32df162f1419e0d9e594
紀衡 黃 <huangchiheng@gmail.com>
2015-01-23 13:41:50 +0800

    建立 plan model 跟 測試檔

--- src/Beyond/PaypalCashier/Plan.php
+++ src/Beyond/PaypalCashier/Plan.php
@@ -1,139 +1,278 @@
 <?php namespace Beyond\PaypalCashier;
 
-use Beyond\Core\BaseEloquentModel;
-use Beyond\PaypalCashier\PlanInterface;
+/*
+     .o8                                                         .o8
+    "888                                                        "888
+     888oooo.   .ooooo.  oooo    ooo  .ooooo.  ooo. .oo.    .oooo888
+     d88' `88b d88' `88b  `88.  .8'  d88' `88b `888P"Y88b  d88' `888
+     888   888 888ooo888   `88..8'   888   888  888   888  888   888
+     888   888 888    .o    `888'    888   888  888   888  888   888
+     `Y8bod8P' `Y8bod8P'     .8'     `Y8bod8P' o888o o888o `Y8bod88P" Inc.
+                         .o..P'
+                         `Y8P'
+ */
 
-class Plan extends BaseEloquentModel implements PlanInterface
+use Illuminate\Database\Eloquent\Model;
+use Beyond\PaypalCashier\Transformers\PlanTransformer;
+use PayPal\Api\PaymentDefinition;
+use PayPal\Api\MerchantPreferences;
+use PayPal\Api\Plan as PaypalPlan;
+
+/**
+ * @author Bryan Huang
+ * @version 0.0.0
+ *
+ * 將 plan 資訊存入到 Database 中
+ * $plan = new Plan // Beyond\Paypal\Plan
+ * $plan->setPaymentDefinitions([$paymentDefination]);
+ * $plan->setMerchantPreference($merchantPreferences)
+ * $plan->save() // create request rest api, save plan info to database
+ */
+class Plan extends Model
 {
-	/**
-	 * @var string
-	 */ 
-	protected $table = 'paypal_plan';
 
-	/**
-	 * Allow to mass assignment
-	 *
-	 * @var array
-	 */ 	
-	protected $fillable = array(
-		'title',
-		'name',
-		'slug',
-		'plan_name',
-		'total_billing_cycle',
-		'billing_period',
-		'billing_frequency',
-		'initamt',
-		'hasTrial',
-		'trial_total_billing_cycle',
-		'trial_billing_period',
-		'trial_billing_frequency'
-	);
+    protected $table = 'paypal_plans';
 
-	/**
-	 * 跟 subscription 的關聯為 one-to-many
-	 *
-	 * @return Illuminate\Database\Eloquent\Relations\BelongsToMany
-	 */ 
-	public function subscription()
-	{
-		return $this->belongsToMany('Beyond\Module\Cashier\PaypalCashier\Subscription', 'id', 'subscription_id');
-	}
+    /**
+     * {@inheritdoc}
+     *
+     * @param array $fillable
+     */
+    protected $fillable = array(
+        'plan_id',
+        'name',
+        'description',
+        'type',
+        'state'
+    );
 
-	/**
-	 * 取得 plan name
-	 * 
-	 * @return string
-	 */ 
-	public function getPlanName()
-	{
-		return $this->plan_name;
-	}	
+    /**
+     * Instance of Paypal\Api\Plan
+     *
+     * @var Paypal\Api\Plan
+     */
+    protected $paypalPlan;
 
-	/**
-	 * 取得 total billing cycle
-	 *
-	 * @return string
-	 */ 
-	public function getTotalBillingCycle()
-	{
-		return $this->total_billing_cycle;
-	}
+    /**
+     * Construct.
+     *
+     * @param array $attributes
+     */
+    public function __construct(array $attributes = array())
+    {
+        // init model attributes
+        parent::__construct($attributes);
 
-	/**
-	 * 取得 billing period
-	 * 1. month
-	 * 2. year
-	 *
-	 * @return string
-	 */ 
-	public function getBillingPeriod()
-	{
-		return $this->billing_period;
-	}
+        $this->setSdkPlan(
+            $this->getNewPaypalPlan($attributes)
+        );
 
-	/**
-	 * 取得 billing frequency
-	 *
-	 * @return string
-	 */ 
-	public function getBillingFrequency()
-	{
-		return $this->billing_frequency;
-	}
+    }
 
-	/**
-	 * 判斷此 plan 是否有 trial period
-	 *
-	 * @return boolean
-	 */ 
-	public function hasTrial()
-	{
-		return $this->hasTrial;
-	}
+    /**
+     * Get the instance of Paypal\Api\Plan.
+     *
+     * @param array $attributes
+     * @return Paypal\Api\PaypalPlan
+     */
+    protected function getNewPaypalPlan(array $attributes = array())
+    {
 
-	/**
-	 * 取得起始的一次性付款數額
-	 *
-	 * @todo 這個數額應該經由計算得出而不是直接 return
-	 * @return string 
-	 */ 
-	public function getInitAmount()
-	{
-		return $this->initamt;
-	}
+        $paypalPlan = new PaypalPlan;
 
-	/**
-	 * 取得 amount
-	 *
-	 * @return float 
-	 */ 
-	public function getAmount()
-	{
-		return $this->amount;
-	}
+        if(!empty($attributes))  $this->initPaypalPlan($paypalPlan, $attributes);
 
-	/**
-	 * 獲取 trial info
-	 *
-	 * @return array
-	 */ 
-	public function fetchTrialInfo()
-	{
-		return array(
-			'TRIALBILLINGPERIOD'=>	$this->trial_billing_period, // 試用期一個 cycle 是一個月
-			'TRIALBILLINGFREQUENCY'=> $this->trial_billing_frequency, 	// 計費頻率
-			'TRIALTOTALBILLINGCYCLES'=> $this->trial_total_billing_cycle,  // 幾個 cycle
-		);
-	}
+        return $paypalPlan;
+    }
 
-	/**
-	 * 取得指定的 plan，取得第一個 符合的 plan
-	 *
-	 * @param Beyond\Module\Cashier\PaypalCashier\PlanInterface
-	 */ 
-	public static function getPlan($name)
-	{
-		return static::where('plan_name', $name)->first();
-	}
+    /**
+     * Set up PayPal\Api\Plan according to
+     *
+     * @param Paypal\Api\PaypalPlan
+     * @param array $attributes
+     * @return void
+     */
+    protected function initPaypalPlan($paypalPlan, $attributes)
+    {
+        foreach ($attributes as $index => $value) {
+            $method = ucfirst($index);
+            if (method_exists($paypalPlan, "set" . $method)) {
+                $paypalPlan->{"set{$method}"}($value);
+            }
+        }
+    }
+
+    /**
+     * Initialize a new Beyond\PaypalCashier\Plan.
+     *
+     * @param array $attributes
+     * @return Beyond\PaypalCashier\Plan
+     */
+    public function newPlan(array $attributes = array())
+    {
+        // 取得新的 Paypal\Api\PaypalPlan
+        $paypalPlan = $this->getNewPaypalPlan();
+
+        $newPlan = $this->newInstance($attributes);
+
+        $newPlan->setSdkPlan($paypalPlan);
+
+        return $newPlan;
+    }
+
+    /**
+     * Set Paypal\Api\Plan object.
+     *
+     * @param Paypal\Api\Plan
+     */
+    public function setSdkPlan($paypalPlan)
+    {
+        $this->paypalPlan = $paypalPlan;
+    }
+
+    /**
+     * Get Paypal sdk Plan instance.
+     *
+     * @return Paypal\Api\Plan
+     */
+    public function getSdkPlan()
+    {
+        return $this->paypalPlan;
+    }
+
+    /**
+     * Set payment defination object. Used for chain calling.
+     *
+     * @param PayPal\Api\PaymentDefination
+     * @return Beyond\PaypalCashier\Plan
+     */
+    public function withPaymentDefinations(PaymentDefinition $paymentDefination)
+    {
+        $this->paypalPlan->setPaymentDefinitions([$paymentDefination]);
+
+        return $this;
+    }
+
+    /**
+     * Set merchant preference object Used for chain calling.
+     *
+     * @param Paypal\Api\MerchantPreference
+     * @return Beyond\PaypalCashier\Plan
+     */
+    public function withMerchantPreferences(MerchantPreferences $merchantPreference)
+    {
+        $this->paypalPlan->setMerchantPreferences($merchantPreference);
+
+        return $this;
+    }
+
+    /**
+     * Set the name of the plan.
+     *
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->getSdkPlan()->setName($name);
+    }
+
+    /**
+     * Get the name of the plan.
+     *
+     * @return string $name
+     */
+    public function getName()
+    {
+        return $this->getSdkPlan()->getName();
+    }
+
+    /**
+     * Set the description of the plan.
+     *
+     * @param string $description
+     */
+    public function setDescription($description)
+    {
+        $this->getSdkPlan()->setDescription($description);
+    }
+
+    /**
+     * Get the description of the plan
+     *
+     * @return string $description
+     */
+    public function getDescription()
+    {
+        return $this->getSdkPlan()->getDescription();
+    }
+
+    /**
+     * Set the type of the plan.
+     *
+     * @param string $type
+     */
+    public function setType($type)
+    {
+        $this->getSdkPlan()->setType($type);
+    }
+
+    /**
+     * Get the type of the plan.
+     *
+     * @return string $type
+     */
+    public function getType()
+    {
+        return $this->getSdkPlan()->getType();
+    }
+
+    /**
+     * Get the unique id of the plan returned by Paypal restful api when plan has been successfully created.
+     *
+     * @return string $id
+     */
+    public function getId()
+    {
+        return $this->getSdkPlan()->getId();
+    }
+
+    /**
+     * Create a new plan.
+     *
+     * @todo Handle errors
+     * @param Paypal\Rest\ApiContext
+     * @return Paypal\Api\Plan
+     */
+    public function createPlan($apiContext)
+    {
+        // request api 取得 plan
+        $plan = $this->getSdkPlan()->create($apiContext);
+
+        // 轉換資料格式
+        $attributes = PlanTransformer::transform($plan);
+
+        $this->fill($attributes)->save();
+
+        return $this;
+    }
+
+    /**
+     * Update plan information. It seems one the state can be updated. Thus, as of Database, we only need to update "state"
+     *
+     * @param
+     */
+    public function updatePlan($pathRequest, $apiContext)
+    {
+        // request paypal restful api
+        // get Paypal\Api\Plan
+        $apiPlan= $this->getSdkPlan()->update($pathRequest, $apiContext);
+
+        // sync new plan
+        $attributes = PlanTransformer::transform($plan);
+//        $attributes = $this->syncPlan($apiPlan);
+
+        // save instance
+        $this->fill($attributes)->save();
+
+    }
 }
---
+++ tests/PaypalRestfulSdkModelTests/PaypalPlanRestfulModelTest.php
@@ -0,0 +1,209 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Beyond\PaypalCashier\Plan;
+use Mockery as m;
+use PayPal\Api\ChargeModel;
+use PayPal\Api\PaymentDefinition;
+use PayPal\Api\MerchantPreferences;
+use PayPal\Api\Currency;
+use PayPal\Rest\ApiContext;
+use PayPal\Auth\OAuthTokenCredential;
+
+class PaypalPlanRestfulSdkTest extends TestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->migratePaypalPlansTable();
+    }
+
+    protected function migratePaypalPlansTable()
+    {
+        Schema::create('paypal_plans', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('plan_id')->nullable();
+            $table->string('name');
+            $table->string('description');
+            $table->string('type');
+            $table->string('state')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    protected function tearDown()
+    {
+        Schema::drop('paypal_plans');
+    }
+
+    /**
+     * Provide sample PayPal\Api\PaymentDefination instance.
+     *
+     * @return PayPal\Api\PaymentDefination
+     */
+    protected function paymentDefinationProvider()
+    {
+        // # Payment definitions for this billing plan.
+        $paymentDefinition = new PaymentDefinition();
+
+        // The possible values for such setters are mentioned in the setter method documentation.
+        // Just open the class file. e.g. lib/PayPal/Api/PaymentDefinition.php and look for setFrequency method.
+        // You should be able to see the acceptable values in the comments.
+        $paymentDefinition->setName('Regular Payments')
+            ->setType('REGULAR')
+            ->setFrequency('Month')
+            ->setFrequencyInterval("2")
+            ->setCycles("12")
+            ->setAmount(new Currency(array('value' => 100, 'currency' => 'USD')));
+
+        // Charge Models
+        $chargeModel = new ChargeModel();
+        $chargeModel->setType('SHIPPING')
+            ->setAmount(new Currency(array('value' => 10, 'currency' => 'USD')));
+
+        $paymentDefinition->setChargeModels(array($chargeModel));
+
+        return $paymentDefinition;
+    }
+
+    protected function merchantPreferenceProvider()
+    {
+        $merchantPreferences = new MerchantPreferences();
+
+        // $baseUrl = getBaseUrl();
+        // ReturnURL and CancelURL are not required and used when creating billing agreement with payment_method as "credit_card".
+        // However, it is generally a good idea to set these values, in case you plan to create billing agreements which accepts "paypal" as payment_method.
+        // This will keep your plan compatible with both the possible scenarios on how it is being used in agreement.
+        $merchantPreferences->setReturnUrl("http://www.return.com")
+            ->setCancelUrl("http://www.cancel.com")
+            ->setAutoBillAmount("yes")
+            ->setInitialFailAmountAction("CONTINUE")
+            ->setMaxFailAttempts("0")
+            ->setSetupFee(new Currency(array('value' => 1, 'currency' => 'USD')));
+
+        return $merchantPreferences;
+    }
+
+    /**
+     * ApiContext instance provider.
+     *
+     * @return PayPal\Api\
+     */
+    protected function apiContextProvider()
+    {
+        $apiContext = new ApiContext(
+            new OAuthTokenCredential(
+                'AW3K_xAvssx_6NKlwND8AkOdilveb2t1n9qtcrc5DHxfvnnrME3u9uPqI9gv',
+                'EJRSABDIeN15U2zw32Ai0YlB2IruSjRetDDG2eiK7MwqoWR6mDlgZ5xXaOq2'
+            )
+        );
+
+        $apiContext->setConfig(
+            array(
+                'mode' => 'sandbox',
+                'log.LogEnabled' => true,
+                'log.FileName' => '../PayPal.log',
+                'log.LogLevel' => 'FINE',
+                'validation.level' => 'log',
+                'cache.enabled' => true,
+                // 'http.CURLOPT_CONNECTTIMEOUT' => 30
+                // 'http.headers.PayPal-Partner-Attribution-Id' => '123123123'
+            )
+        );
+
+        return $apiContext;
+    }
+
+    public function test_paypal_plan_init()
+    {
+        $plan = new Plan;
+
+        $this->assertInstanceOf('Beyond\PaypalCashier\Plan', $plan);
+    }
+
+    public function test_make_api_context_obj()
+    {
+
+    }
+    
+    /**
+     * Test create new plan.
+     *
+     * Tests:
+     *      1. Test attributes have been set to Beyond\PaypalCashier\Plan
+     *      2. Test Sdk plan attributes has been set
+     */
+    public function test_init_new_plan()
+    {
+        // create a new plan instance
+        $plan = new Plan([
+            'name'          =>  'sample plan',
+            'type'          =>  'fixed',
+            'description'   =>  'sample plan'
+        ]);
+
+        // 1.
+        $this->assertEquals($plan->name, 'sample plan');
+        $this->assertEquals($plan->type, 'fixed');
+        $this->assertEquals($plan->description, 'sample plan');
+
+        // 2.
+
+        $this->assertEquals($plan->getName(), 'sample plan');
+        $this->assertEquals($plan->getType(), 'fixed');
+        $this->assertEquals($plan->getDescription(), 'sample plan');
+    }
+
+    /**
+     * Test plan model create new plan.
+     *
+     * Tests:
+     *      1. Makesure plan id has been set.
+     *      2. Makesure plan id has been saved into database.
+     */
+    public function test_create_new_plan()
+    {
+        $plan = new Plan([
+            'name'          =>  'sample plan',
+            'type'          =>  'fixed',
+            'description'   =>  'sample plan'
+        ]);
+
+        $paymentDefinition = $this->paymentDefinationProvider();
+
+        $merchantPreference = $this->merchantPreferenceProvider();
+
+        $apiContext = $this->apiContextProvider();
+
+        $plan  = $plan->withPaymentDefinations($paymentDefinition)->withMerchantPreferences($merchantPreference)->createPlan($apiContext);
+
+        // 1.
+        $this->assertNotNull($plan->getId());
+
+        // 2.
+        $count = DB::table('paypal_plans')->where('plan_id',  $plan->getId())->count();
+        $this->assertEquals(1, $count);
+
+    }
+
+    /**
+     *
+     *
+     *
+     */
+    public function test_update_existed_plan()
+    {
+
+    }
+
+    public function test_plan_repository_save_plan_method()
+    {
+        $plan = new Plan;
+
+        $plan->savePlan($information);
+
+    }
+}
